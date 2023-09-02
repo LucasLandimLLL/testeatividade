91ff18c725d3d9fd983187cf6b95e8288986f0d8
LucasLandimLLL <lucas.landim2510@gmail.com>
2023-09-01 21:30:09 -0300

    funcional texto mudando

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Teste } from './componentes/Button';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 
 
 export default function App() {
+const [frases, setFrases]=useState()
+
+const botaoPressionado = () => {
+  var numeroAleatorio = Math.random();
+  numeroAleatorio = Math.floor(numeroAleatorio*5);
+
+  var frases = Array();
+  frases[0] = "vai cu";
+  frases[1] = "O senhor é meu pastor e nada faltara";
+  frases[2] = "Amigo de todos, Amigo de ninguem";
+  frases[3] = "Em terra de cego quem tem um olho é rei";
+  frases[4] = "Há pessoas que caminham pela floresta e so encherga LENHA";
+
+  var fraseEscolhida = frases[numeroAleatorio];
+ setFrases(fraseEscolhida)
+};
+
   return (
     <View style={styles.container}>
-  <Teste/>
-</View>
+      <Text>{frases}</Text>
+ <Button onPress={botaoPressionado} title="Sorte"></Button>
+ <Button onPress={botaoPressionado} title="Sorte"></Button>
+    </View>
   );
 }
 
@@ -22,4 +41,3 @@ const styles = StyleSheet.create({
     color:"white",
   },
 });
-
